b842ce144c9d1733106123adddd165ffa153456e
Bernat Borrás <bernat.borras@adevinta.com>
2026-03-13 20:26:13 +0100

    Switch publishing to Gradle Plugin Portal via plugin-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.maven.publish)
-    `java-gradle-plugin`
+    alias(libs.plugins.plugin.publish)
 }
 
 group = "com.github.alorma"
@@ -27,47 +24,17 @@ tasks.test {
 }
 
 gradlePlugin {
+    website = "https://github.com/alorma/VersionPlugin"
+    vcsUrl = "https://github.com/alorma/VersionPlugin"
+
     plugins {
         create("versionPlugin") {
             id = "com.github.alorma.version"
             implementationClass = "VersionPlugin"
             displayName = "Version Plugin"
             description = "Automatically configure Android app versioning from a version.properties file"
+            tags = listOf("android", "versioning")
         }
     }
     testSourceSets(sourceSets["test"])
-}
-
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-    signAllPublications()
-
-    coordinates("com.github.alorma", "version-plugin", "1.0.0")
-
-    pom {
-        name.set("Version Plugin")
-        description.set("Automatically configure Android app versioning from a version.properties file")
-        url.set("https://github.com/alorma/VersionPlugin")
-
-        licenses {
-            license {
-                name.set("Apache License 2.0")
-                url.set("https://www.apache.org/licenses/LICENSE-2.0")
-            }
-        }
-
-        developers {
-            developer {
-                id.set("alorma")
-                name.set("Bernat Borrás")
-                email.set("bernatbor15@gmail.com")
-            }
-        }
-
-        scm {
-            connection.set("scm:git:github.com/alorma/VersionPlugin.git")
-            developerConnection.set("scm:git:ssh://github.com/alorma/VersionPlugin.git")
-            url.set("https://github.com/alorma/VersionPlugin/tree/main")
-        }
-    }
 }
